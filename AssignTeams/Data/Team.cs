412ca3c9586addc937dc2a68ebd731f7d2dc71cd
Rebekah Colon <Rebekah.Colon@fsresidential.com>
2017-07-19 22:00:18 -0400

    Removed unnecessary sample pages and added initial template for team generation. Added POCOs for entities Associate and Team

---
+++ AssignTeams/Data/Team.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssignTeams.Data
+{
+    public class Team
+    {
+        public int Number { get; set; }
+        public List<Associate> Members { get; set; }
+
+        public Team()
+        {
+            Members = new List<Associate>();
+        }
+    }
+}
