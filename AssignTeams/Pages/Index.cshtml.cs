8e8734721e1f2a438595cd94479452da507d9108
Rebekah Colon <Rebekah.Colon@fsresidential.com>
2017-09-04 15:25:31 -0400

    Made a few changes to start connecting the UI to the Service

--- AssignTeams/Pages/Index.cshtml.cs
+++ AssignTeams/Pages/Index.cshtml.cs
@@ -4,14 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using AssignTeams.Core.Models;
+using AssignTeams.Core;
 
 namespace AssignTeams.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ITeamGeneratorService _generatorService;
 
+        public IndexModel()
+        {
+            _generatorService = new TeamGeneratorService();
+        }
+
+        [BindProperty]
+        public GeneratorParams GeneratorParameters { get; set; }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            try
+            {
+                GeneratorParameters.People.Add(new Person { Name = "Becks" });
+                GeneratorParameters.People.Add(new Person { Name = "B" });
+                GeneratorParameters.People.Add(new Person { Name = "R" });
+                GeneratorParameters.People.Add(new Person { Name = "Colon" });
+                _generatorService.Run(GeneratorParameters);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return RedirectToPage("./ShowTeams");
         }
     }
 }
